b1b28262a32a468dcaf9649e6fb417cd431433cd
Aditya Pandey <130052755+addyhacke@users.noreply.github.com>
2024-06-08 12:23:25 +0000

    studied about primitve and non primitve datatpe

--- datatype.js
+++ datatype.js
@@ -7,3 +7,21 @@ console.log(3+4);
 console.log(typeof(a));
 console.log(typeof(null));
 console.log(typeof(undefined));
+
+
+
+// Primitive Data Types
+let str = "Hello"; // String
+let num = 42; // Number
+let bool = true; // Boolean
+let n = null; // null
+let undef = undefined; // undefined
+let sym = Symbol("foo"); // Symbol
+let bigIntVal = 1234567890123456789012345678901234567890n; // BigInt
+
+// Reference (Non-Primitive) Data Types
+let arr = [1, 2, 3]; // Array
+let obj = { key: "value" }; // Object
+function func() { console.log("Hello!"); } // Function
+
+console.log(typeof(bigInt));
