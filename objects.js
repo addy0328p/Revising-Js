c224b28496b7f427760175672717c05509801883
Aditya Pandey <130052755+addyhacke@users.noreply.github.com>
2024-06-12 17:53:47 +0000

    revise concepts like destructuring JSON constructors

--- objects.js
+++ objects.js
@@ -3,12 +3,12 @@ const mySym = Symbol("key1");
 
 // Create an object using object literals
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary", // Property with space in the name
+    name: "Aditya",
+    "full name": "Aditya Pandey", // Property with space in the name
     [mySym]: "mykey1", // Property with a symbol as the key
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "lko",
+    email: "addy@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"] // Array as a property value
 };
@@ -44,3 +44,104 @@ JsUser.greetingTwo = function() {
 // Calling the methods
 console.log(JsUser.greeting()); // Output: Hello JS user
 console.log(JsUser.greetingTwo()); // Output: Hello JS user, Hitesh
+
+
+
+// *********************OBJECTS-PART-2*************************
+
+
+
+
+// Creating an empty object using object literal notation
+// const tinderUser = new Object()
+const tinderUser = {};
+
+// Adding properties to the tinderUser object
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+// Log the tinderUser object to the console
+// console.log(tinderUser); // Output: { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+};
+
+// Accessing nested properties
+// console.log(regularUser.fullname.userfullname.firstname); // Output: hitesh
+
+// Creating objects obj1, obj2, and obj4
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
+
+// Merging obj1, obj2, and obj4 into a new object obj3 using spread operator
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = { ...obj1, ...obj2 };
+
+// Log the merged object to the console
+// console.log(obj3); // Output: { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2, // Changed to 2 to avoid duplicate id
+        email: "h2@gmail.com" // Changed to avoid duplicate email
+    },
+    {
+        id: 3, // Changed to 3 to avoid duplicate id
+        email: "h3@gmail.com" // Changed to avoid duplicate email
+    }
+];
+
+// Accessing the email property of the second user
+// console.log(users[1].email); // Output: h2@gmail.com
+
+// Log the tinderUser object to the console
+// console.log(tinderUser); // Output: { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+// Getting and logging the keys, values, and entries of the tinderUser object
+// console.log(Object.keys(tinderUser)); // Output: [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); // Output: [ '123abc', 'Sammy', false ]
+// console.log(Object.entries(tinderUser)); // Output: [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+// Checking if the tinderUser object has a property 'isLoggedIn'
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // Output: true
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "addy"
+};
+
+// Destructuring the course object to extract the courseInstructor property
+const { courseInstructor: instructor } = course;
+
+// Log the instructor variable to the console
+// console.log(courseInstructor); // Error: courseInstructor is not defined
+console.log(instructor); // Output: hitesh
+
+// Example of JSON-like object
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// Example of an array of empty objects
+[
+    {},
+    {},
+    {}
+]
