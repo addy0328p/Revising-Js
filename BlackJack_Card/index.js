8ca6c945b6da5b2245d6a275b7be3a5e8bf3724f
Aditya Pandey <aditya12bone@gmail.com>
2025-02-01 20:35:04 +0530

    Update index.js

--- BlackJack_Card/index.js
+++ BlackJack_Card/index.js
@@ -48,6 +48,7 @@ function renderGame() {
 
 
 function newCard() {
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
     let card = getRandomCard()
     sum += card
     cards.push(card)
